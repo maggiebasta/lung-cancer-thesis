2c6c8fe5ae038adc3705a38f9af2d69ec59010b5
Ubuntu <ubuntu@ip-172-31-1-5.us-east-2.compute.internal>
2019-11-30 18:00:37 +0000

    interp 3d

---
+++ unet_3d_interp/data_downloader.py
@@ -0,0 +1,251 @@
+import os
+import pickle
+import shutil
+import sys
+
+import boto3
+import cv2 as cv
+import pydicom
+import numpy as np
+
+from scipy import ndimage, misc
+from skimage.io import imsave
+from sklearn.model_selection import train_test_split
+from skimage.transform import rescale
+
+from lidc_helpers import (
+    find_ct_path,
+    get_mask,
+    get_patient_df_v2,
+    get_series_uid
+)
+
+"""
+Data Downloader:
+Downloads and prepares raw LIDC-IDRI data from S3 storage
+"""
+
+
+def get_s3_keys(prefix, bucket='mbasta-thesis-2019'):
+    """
+    Gets a list of all keys in S3 bucket with prefix
+
+    :param prefix: prefix of keys to return
+    :bucket: optional, S3 bucket to get keys from
+    :return: list of s3 object keys
+    """
+
+    s3 = boto3.client('s3')
+    keys = []
+
+    kwargs = {'Bucket': bucket, 'Prefix': prefix}
+    while True:
+        resp = s3.list_objects_v2(**kwargs)
+        if not resp.get('Contents'):
+            return
+        for obj in resp['Contents']:
+            keys.append(obj['Key'])
+
+        try:
+            kwargs['ContinuationToken'] = resp['NextContinuationToken']
+        except KeyError:
+            break
+    return keys
+
+
+def download_from_s3(keys, dest_path, bucket='mbasta-thesis-2019'):
+
+    """
+    Downloads all keys with prefix from S3
+
+    :param prefix: prefix of keys to return
+    :param dest_folder: where to store downloads, default is 'data/raw/'
+    :bucket: optional, S3 bucket to get keys from
+    :return: None
+    """
+    s3 = boto3.client('s3')
+    for key in keys:
+        dest_file = dest_path + key
+        try:
+            s3.download_file(bucket, key, dest_file)
+        except FileNotFoundError:
+            cur = ""
+            Add = False
+            for i, d in enumerate(dest_file.split('/')[:-1]):
+                if i == 0:
+                    cur = d
+                else:
+                    cur = cur + "/" + d
+                if Add:
+                    os.mkdir(cur)
+                else:
+                    if not os.path.isdir(cur):
+                        os.mkdir(cur)
+                        Add = True
+            s3.download_file(bucket, key, dest_file)
+
+
+def interpolate_and_normalize(image, slice_thickness, pixel_spacing, norm=True):
+
+    # # clip values
+    # image = np.clip(image, a_min=-1200, a_max=600) 
+
+    # interpolate slices
+    image = ndimage.zoom(image, (slice_thickness, pixel_spacing[0], pixel_spacing[1]))
+
+    # normalize image
+    if norm:
+        image = cv.normalize(
+            image,
+            np.zeros(image.shape),
+            0,
+            255,
+            cv.NORM_MINMAX
+        )
+    
+    # resize slices to 256, 256
+    image = [
+        cv.resize(im, dsize=(256, 256))
+        for im in image
+    ]
+
+    # return middle 8 slices
+    start = max(0, int(len(image)/2) - 4)
+    stop = start + 8
+    return np.array(image[start:stop])
+
+
+def _prepare(patient_id, raw_path, prepped_path):
+    """
+    Called from download_transform_split. Given an output path, properly
+    formats raw LIDC-IDRI images into prepared form and saves them in specified
+    output path
+
+    :param patient_id: patient ID of images, in form 'LIDC-IDRI-XXXX'
+    :param raw_path: path to raw data to prepare
+    :param prepped_path: path to directory for prepared data
+    :return: None
+    """
+
+    # check if patient in LUMA
+    uids = pickle.load(open( "uids.pkl", "rb" ))
+    if get_series_uid(find_ct_path(raw_path, patient_id)) not in uids:
+        return 
+
+    # get image and contours for patient images
+    pid_df = get_patient_df_v2(raw_path, patient_id)
+    if type(pid_df) == type(None):
+        return 
+
+    image = [pydicom.dcmread(row[1].path).pixel_array for row in pid_df.iterrows()]
+    rois = [row[1].ROIs for row in pid_df.iterrows()]
+    mask = [get_mask(im, roi) for im, roi in zip(image, rois)]
+
+    slice_thickness = pydicom.dcmread(pid_df.iloc[0].path)[('0018', '0050')].value
+    pixel_spacing = pydicom.dcmread(pid_df.iloc[0].path)[('0028', '0030')].value
+
+    image = interpolate_and_normalize(np.array(image), slice_thickness, pixel_spacing)
+    mask = interpolate_and_normalize(np.array(mask), slice_thickness, pixel_spacing, norm=False)
+    mask[mask > .5] = 1
+    mask[mask <= .5] = 0
+
+    # save prepared image and mask in properly constructed directory
+    while True:
+        try:
+            idx = len(os.listdir(f"{prepped_path}/image1/"))
+            for i in range(8):
+                imsave(f"{prepped_path}/image{i}/{idx}.png", image[i])
+                imsave(f"{prepped_path}/label{i}/{idx}.png", mask[i])
+
+        except FileNotFoundError:
+            if not os.path.isdir(prepped_path):
+                os.mkdir(prepped_path)
+            for i in range(8):
+                os.mkdir(f"{prepped_path}/image{i}")
+                os.mkdir(f"{prepped_path}/label{i}")
+            continue
+        break
+
+
+def test_train_split(datapath, trainpath, testpath):
+    """
+    Called from download_transform_split. Creates training and test sets from
+    prepared images
+
+    :param datapath: the directory containing prepared, train, and test folders
+    :return: None
+    """
+
+    os.mkdir(trainpath)
+    os.mkdir(testpath)
+    for i in range(8):
+        os.mkdir(f"{trainpath}/image{i}")
+        os.mkdir(f"{trainpath}/label{i}")
+        os.mkdir(f"{testpath}/image{i}")
+        os.mkdir(f"{testpath}/label{i}")
+
+    idxs = range(len(os.listdir(f"{datapath}/image0/")))
+    train_idxs, test_idxs = train_test_split(idxs, test_size=.2)
+    for i, idx in enumerate(train_idxs):
+        for j in range(8):
+            im_source = f"{datapath}/image{j}/{idx}.png"
+            im_dest = f"{trainpath}/image{j}/{i}.png"
+            shutil.copyfile(im_source, im_dest)
+
+            msk_source = f"{datapath}/label{j}/{idx}.png"
+            msk_dest = f"{trainpath}/label{j}/{i}.png"
+            shutil.copy(msk_source, msk_dest)
+
+    for i, idx in enumerate(test_idxs):
+        for j in range(8):
+            im_source = f"{datapath}/image{j}/{idx}.png"
+            im_dest = f"{testpath}/image{j}/{i}.png"
+            shutil.copyfile(im_source, im_dest)
+
+            msk_source = f"{datapath}/label{j}/{idx}.png"
+            msk_dest = f"{testpath}/label{j}/{i}.png"
+            shutil.copy(msk_source, msk_dest)
+
+
+def download_and_extract(
+    idx_i,
+    idx_j,
+    raw_path='data/raw/',
+    prepped_path='data/prepared',
+    delete_raw=True
+):
+    """
+    Called from main method. Given a number of samples, downloads and prepares
+    raw data from S3 for the Unet model.
+
+    :param idx_i: index of first patient in range to download from s3
+    :param idx_j: index of last patient in range to download from s3
+    :param raw_path: optional, path for raw data, default='data/raw/'
+    :param preppred_path: optional, path for prepared data, default='data/'
+    :param delete_raw: optional, whether to delete raw data after, default=True
+    """
+    id_nums = [
+        '0'*(4-len(n))+n for n in [str(i) for i in range(idx_i, idx_j+1)]
+    ]
+    ids = [f"LIDC-IDRI/LIDC-IDRI-{id_num}" for id_num in id_nums]
+    for i, pid in enumerate(ids):
+        sys.stdout.write(f"\rPreparing...{i+1}/{idx_j+1-idx_i}")
+        sys.stdout.flush()
+        keys = get_s3_keys(prefix=pid)
+        if keys:
+            download_from_s3(keys, raw_path)
+
+            _prepare(pid, raw_path, prepped_path)
+
+            if delete_raw:
+                shutil.rmtree(f"{raw_path}{pid}/")
+
+    if delete_raw:
+        shutil.rmtree(raw_path)
+
+    print(f"\nComplete.")
+
+
+if __name__ == "__main__":
+    num_patients = int(sys.argv[1])
+    download_transform_split(num_patients)
---
+++ unet_3d_interp/data_generator.py
@@ -0,0 +1,276 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow.keras.preprocessing.image import (
+    ImageDataGenerator,
+    img_to_array,
+    load_img
+)
+import sys
+"""
+Data Loader:
+Loads the membrane cell segmentation dataset
+Adapted and simplified from:
+https://github.com/zhixuhao/unet/blob/master/data.py
+and:
+https://github.com/a-martyn/unet/blob/master/model/data_loader.py
+"""
+
+# Keras Image Data Generator Templates for train and test images and labels
+# ----------------------------------------------------------------------------
+image_generator_train = ImageDataGenerator(
+    rotation_range=2,
+    brightness_range=[0.8, 1.2],
+    rescale=1./255,
+    shear_range=0.05,
+    horizontal_flip=True,
+    fill_mode='reflect',
+    data_format='channels_last',
+    validation_split=0.0
+)
+label_generator_train = ImageDataGenerator(
+    rotation_range=2,
+    # No brightness transform on target mask
+    # No rescale transform on target mask
+    shear_range=0.05,
+    horizontal_flip=True,
+    fill_mode='reflect',
+    data_format='channels_last',
+    validation_split=0.0
+)
+
+image_generator_test = ImageDataGenerator(
+    rescale=1./255,
+    fill_mode='reflect',
+    data_format='channels_last',
+    validation_split=0.0
+)
+
+label_generator_test = ImageDataGenerator(
+    # No rescale transform on target mask
+    fill_mode='reflect',
+    data_format='channels_last',
+    validation_split=0.0
+)
+
+
+# Instantiated joined image and mask generators for model input
+# ----------------------------------------------------------------------------
+
+def generator(directory, input_gen, target_gen, batch_sz=2, img_sz=(256, 256)):
+
+    # Input generators
+    x0_gen = input_gen.flow_from_directory(
+        directory,
+        target_size=img_sz,
+        color_mode="grayscale",
+        classes=['image0'],
+        class_mode=None,
+        batch_size=batch_sz,
+        seed=1,
+        interpolation='nearest'
+    )
+    x1_gen = input_gen.flow_from_directory(
+        directory,
+        target_size=img_sz,
+        color_mode="grayscale",
+        classes=['image1'],
+        class_mode=None,
+        batch_size=batch_sz,
+        seed=1,
+        interpolation='nearest'
+    )
+    x2_gen = input_gen.flow_from_directory(
+        directory,
+        target_size=img_sz,
+        color_mode="grayscale",
+        classes=['image2'],
+        class_mode=None,
+        batch_size=batch_sz,
+        seed=1,
+        interpolation='nearest'
+    )
+    x3_gen = input_gen.flow_from_directory(
+        directory,
+        target_size=img_sz,
+        color_mode="grayscale",
+        classes=['image3'],
+        class_mode=None,
+        batch_size=batch_sz,
+        seed=1,
+        interpolation='nearest'
+    )
+    x4_gen = input_gen.flow_from_directory(
+        directory,
+        target_size=img_sz,
+        color_mode="grayscale",
+        classes=['image4'],
+        class_mode=None,
+        batch_size=batch_sz,
+        seed=1,
+        interpolation='nearest'
+    )
+    x5_gen = input_gen.flow_from_directory(
+        directory,
+        target_size=img_sz,
+        color_mode="grayscale",
+        classes=['image5'],
+        class_mode=None,
+        batch_size=batch_sz,
+        seed=1,
+        interpolation='nearest'
+    )
+    x6_gen = input_gen.flow_from_directory(
+        directory,
+        target_size=img_sz,
+        color_mode="grayscale",
+        classes=['image6'],
+        class_mode=None,
+        batch_size=batch_sz,
+        seed=1,
+        interpolation='nearest'
+    )
+    x7_gen = input_gen.flow_from_directory(
+        directory,
+        target_size=img_sz,
+        color_mode="grayscale",
+        classes=['image7'],
+        class_mode=None,
+        batch_size=batch_sz,
+        seed=1,
+        interpolation='nearest'
+    )
+
+    # Target generators
+    y0_gen = input_gen.flow_from_directory(
+        directory,
+        target_size=img_sz,
+        color_mode="grayscale",
+        classes=['label0'],
+        class_mode=None,
+        batch_size=batch_sz,
+        seed=1,
+        interpolation='nearest'
+    )
+    y1_gen = input_gen.flow_from_directory(
+        directory,
+        target_size=img_sz,
+        color_mode="grayscale",
+        classes=['label1'],
+        class_mode=None,
+        batch_size=batch_sz,
+        seed=1,
+        interpolation='nearest'
+    )
+    y2_gen = input_gen.flow_from_directory(
+        directory,
+        target_size=img_sz,
+        color_mode="grayscale",
+        classes=['label2'],
+        class_mode=None,
+        batch_size=batch_sz,
+        seed=1,
+        interpolation='nearest'
+    )
+    y3_gen = input_gen.flow_from_directory(
+        directory,
+        target_size=img_sz,
+        color_mode="grayscale",
+        classes=['label3'],
+        class_mode=None,
+        batch_size=batch_sz,
+        seed=1,
+        interpolation='nearest'
+    )
+    y4_gen = input_gen.flow_from_directory(
+        directory,
+        target_size=img_sz,
+        color_mode="grayscale",
+        classes=['label4'],
+        class_mode=None,
+        batch_size=batch_sz,
+        seed=1,
+        interpolation='nearest'
+    )
+    y5_gen = input_gen.flow_from_directory(
+        directory,
+        target_size=img_sz,
+        color_mode="grayscale",
+        classes=['label5'],
+        class_mode=None,
+        batch_size=batch_sz,
+        seed=1,
+        interpolation='nearest'
+    )
+    y6_gen = input_gen.flow_from_directory(
+        directory,
+        target_size=img_sz,
+        color_mode="grayscale",
+        classes=['label6'],
+        class_mode=None,
+        batch_size=batch_sz,
+        seed=1,
+        interpolation='nearest'
+    )
+    y7_gen = input_gen.flow_from_directory(
+        directory,
+        target_size=img_sz,
+        color_mode="grayscale",
+        classes=['label7'],
+        class_mode=None,
+        batch_size=batch_sz,
+        seed=1,
+        interpolation='nearest'
+    )
+
+    generator = zip(
+        x0_gen, x1_gen, x2_gen, x3_gen, x4_gen, x5_gen, x6_gen, x7_gen,
+        y0_gen, y1_gen, y2_gen, y3_gen, y4_gen, y5_gen, y6_gen, y7_gen
+    )
+    # generator = zip(
+    #     x0_gen, x1_gen, x2_gen, x3_gen,
+    #     y0_gen, y1_gen, y2_gen, y3_gen
+    # )
+    for (x0, x1, x2, x3, x4, x5, x6, x7, y0, y1, y2, y3, y4, y5, y6, y7) in generator:
+        X = np.array([x0, x1, x2, x3, x4, x5, x6, x7]).reshape(batch_sz, 256, 256, 8, 1)
+        Y = np.array([y0, y1, y2, y3, y4, y5, y6, y7]).reshape(batch_sz, 256, 256, 8, 1)
+        yield (X, Y)
+    # for (x0, x1, x2, x3, y0, y1, y2, y3) in generator:
+    #     X = np.array([x0, x1, x2, x3]).reshape(batch_sz, 256, 256, 4, 1)
+    #     Y = np.array([y0, y1, y2, y3]).reshape(batch_sz, 256, 256, 4, 1)
+    #     yield (X, Y)
+
+# Data visualization
+# ----------------------------------------------------------------------------
+
+def show_augmentation(img_filepath, imageDataGenerator, n_rows=1):
+    n_cols = 4
+    img = load_img(img_filepath)
+    x = img_to_array(img)
+    x = x.reshape((1,) + x.shape)
+
+    fig = plt.figure(figsize=(16, 8))
+    i = 1
+    for batch in imageDataGenerator.flow(x, batch_size=1, seed=1):
+        ax = fig.add_subplot(n_rows, n_cols, i)
+        ax.imshow(batch[0])
+        ax.axis('off')
+        i += 1
+        if i > n_rows*n_cols:
+            break
+    plt.show()
+    return
+
+
+def show_sample(generator):
+    batch = next(generator)
+    x = batch[0][0]
+    y = batch[1][0]
+
+    size = (5, 5)
+    plt.figure(figsize=size)
+    plt.imshow(x[:, :, 0], cmap='gray')
+    plt.show()
+    plt.figure(figsize=size)
+    plt.imshow(y[:, :, 0], cmap='gray')
+    plt.show()
+    return
