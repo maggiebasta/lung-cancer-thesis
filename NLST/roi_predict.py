bb468c94f61ba277ec044cb56cc2dbd083cf8924
Ubuntu <ubuntu@ip-172-31-1-5.us-east-2.compute.internal>
2020-02-20 19:33:33 +0000

    nlst update

--- NLST/roi_predict.py
+++ NLST/roi_predict.py
@@ -36,6 +36,28 @@ def get_most_activated_roi(y_pred_mask):
     return [(x_center-25, y_center-25), (x_center+25, y_center+25)]
 
 
+def get_most_activated_roi_mask(y_pred_mask, shape=256):
+    if y_pred_mask.max() < .1:
+        return y_pred_mask
+    y_pred_mask = y_pred_mask.reshape(shape, shape)
+    blobs = y_pred_mask > .1
+    blobs_labels = measure.label(blobs, background=0)
+    labels = np.unique(blobs_labels)[1:]
+    activations = []
+    for label in labels:
+        blob_activation = 0.
+        idxs = np.argwhere(blobs_labels == label)
+        for idx_x, idx_y in idxs:
+            blob_activation += y_pred_mask[idx_x][idx_y]
+        activations.append(blob_activation/len(idxs))
+    new_pred_mask = np.zeros(y_pred_mask.shape)
+    for i in range(256):
+        for j in range(256):
+            if blobs_labels[i][j] == np.argmax(activations) + 1:
+                new_pred_mask[i][j] = y_pred_mask[i][j]
+    return new_pred_mask
+
+
 def get_rois(extracted_path, processed_path, roi_2d_path, roi_3d_path, model):
     """
     Given a model and original images, predicts and saves the 2D (50 x 50) and
@@ -50,9 +72,11 @@ def get_rois(extracted_path, processed_path, roi_2d_path, roi_3d_path, model):
     os.mkdir(roi_3d_path)
     pids = os.listdir(processed_path)
     n = len(pids)
-    Areas = {}
+    Extracted = {}
     for i, pid in enumerate(pids):
         max_area = 0
+        perimeter = 0
+        diameter = 0
         sys.stdout.write(f"\rGetting ROIs...{i+1}/{n}")
         sys.stdout.flush()
         for im_path in os.listdir(processed_path + '/' + str(pid)):
@@ -85,9 +109,6 @@ def get_rois(extracted_path, processed_path, roi_2d_path, roi_3d_path, model):
                 mins, maxs = get_most_activated_roi(nodule_pred)
                 xmin, ymin = mins
                 xmax, ymax = maxs
-                mins, maxs = get_most_activated_roi(nodule_pred)
-                xmin, ymin = mins
-                xmax, ymax = maxs
                 predicted_roi = np.array([
                     normalize(slc)[xmin:xmax][:, ymin:ymax]
                     for slc in cube
@@ -96,9 +117,26 @@ def get_rois(extracted_path, processed_path, roi_2d_path, roi_3d_path, model):
                     os.mkdir(roi_3d_path + '/' + str(pid))
                 outpath = f"{roi_3d_path}/{pid}/{im_path[:-4]}.pkl"
                 pkl.dump(predicted_roi, open(outpath, "wb"))
+
+                new_pred = get_most_activated_roi_mask(nodule_pred, new_dims)
+                new_pred[new_pred > .1] = 255
+                new_pred[new_pred < .1] = 0
+                new_pred = new_pred.reshape(new_dims, new_dims).astype('uint8')
+                if max_area < new_pred.sum():
+                    max_area = new_pred.sum()/255
+                    perimeter = cv.Canny(new_pred, 100, 200).sum()/255
+                    coords = np.argwhere(new_pred > 0)
+                    diameter = 0
+                    for i, (x1, y1) in enumerate(coords):
+                        for x2, y2 in coords[i+1:]:
+                            diameter = max(
+                                diameter,
+                                np.sqrt((x1-x2)**2+(y1-y2)**2)
+                            )
+
             except (ValueError, IndexError):
                 sys.stdout.write(f"\nNo predicted ROI for {pid} {im_path}\n")
                 pass
-        Areas[pid] = max_area
-    pkl.dump(Areas, open('data/areas.pkl', "wb"))
+        Extracted[pid] = max_area, perimeter, diameter
+    pkl.dump(Extracted, open('data/geometric_data.pkl', "wb"))
     print(f"\nComplete.")
